2ebeac0d859828035179ba5c40a405e8f0e0f3f1
Cristagolem <cristagole.b@gmail.com>
2020-12-18 19:58:40 +0100

    Annotazione "NonNullo"
    Atta ad eliminare la dipendenza da Lombok

---
+++ randomengine.randomdevs.org/NotNull.java
@@ -0,0 +1,10 @@
+package randomengine.randomdevs.org;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target(value={ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE})
+@Retention(value=RetentionPolicy.CLASS)
+public @interface NotNull {}
