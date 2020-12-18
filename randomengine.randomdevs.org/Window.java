c22bad2c7c63d33e8882b6b63683c241ef545a58
Cristagolem <cristagole.b@gmail.com>
2020-12-18 21:48:50 +0100

    Update Window.java
    rearrangiamento del codice, implementazione constructor

--- randomengine.randomdevs.org/Window.java
+++ randomengine.randomdevs.org/Window.java
@@ -1,32 +1,43 @@
 package randomengine.randomdevs.org;
 
-import java.awt.event.WindowEvent;
+import java.awt.Canvas;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
-public class Window {
-
-	public static JFrame newWindow(@NotNull String frameName, @NotNull int width, int height[]) {																										// teoria
-		JFrame frame = new JFrame(frameName); // nuovo Frame di JFrame, non usiamo Window perché è più macchinoso
-		if (height.length == 1) {
-			frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		} else {
-			frame.setDefaultCloseOperation(height[1]); // altrimenti usa l'argomento fornito
-		}
-		frame.setSize(width, height[0]); // impostiamo la grandezza della finestra
-		return frame; // restituiamo la finestra (altrimenti non sarebbe un costruttore)
+public class Window extends Canvas {
+	
+	
+	private static final long serialVersionUID = 1L;
+	
+	String frameName;
+	int width;
+	int height;
+	int maxWidth;
+	int maxHeight;
+	int defCloseOperation;
+	boolean resizable;
+	
+	public Window(String frameName, int width[], int height[], boolean resizable) {
+		this.frameName = frameName;
+		this.width = width[0];
+		this.height = height[0];
+		this.maxWidth = width[1];
+		this.maxHeight = height[1];
+		this.defCloseOperation = JFrame.DISPOSE_ON_CLOSE;
+		this.resizable = resizable;
+		
+		JFrame frame = new JFrame(frameName);
+		frame.setMinimumSize(new Dimension(width[0], height[0]));
+		frame.setMaximumSize(new Dimension(width[1], height[1]));
+		frame.setResizable(resizable);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setVisible(true);
 	}
 	
-	public static JFrame newWindow(@NotNull String frameName, @NotNull int width, int height) {																										// teoria
-		JFrame frame = new JFrame(frameName); // nuovo Frame di JFrame, non usiamo Window perché è più macchinoso
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.setSize(width, height); // impostiamo la grandezza della finestra
-		return frame; // restituiamo la finestra (altrimenti non sarebbe un costruttore)
-	}
-
-	public static void obliterate(@NotNull JFrame frame) { 
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.setVisible(false);
-		frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSED)); 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
